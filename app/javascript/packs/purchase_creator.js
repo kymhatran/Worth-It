dda6ae16e3781c638a544764cd9363caa5a35c76
Kym hatran <kym.hatran@gmail.com>
2021-07-24 10:44:57 +1000

    calculator

--- app/javascript/packs/purchase_creator.js
+++ app/javascript/packs/purchase_creator.js
@@ -7,14 +7,26 @@ function calculator(){
     event.preventDefault()
     const impact = document.getElementById("impact")
     const setback = document.getElementById("setback")
+    const context = document.getElementById("context")
+    const visual = document.getElementById("visual")
+    const goal_name = document.getElementById("goal_name").innerHTML
+    const goal_reason = document.getElementById("goal_reason").value
+
     const days_reason = document.getElementById("days_reason")
     const purchase = purchaseForm.querySelectorAll("input")[0].value
     const cost = purchaseForm.querySelectorAll("input")[1].value
+
     const calculatedImpact = (parseInt(cost) / parseInt(weeklyContribution)) * 7
 
+    const coffees= (parseInt(cost) / 5)
+    const object= `☕️`;
+
+
     impact.innerHTML = `If you buy this ${purchase} for $${cost}:`
-    setback.innerHTML = `⭐️ You won’t achieve your {insert savings goal} for another ${calculatedImpact} days`
-    days_reason.innerHTML = `📆 That's ${calculatedImpact} days without your {reason for saving}`
+    setback.innerHTML = `⭐️ You won’t achieve your ${goal_name} for another ${calculatedImpact} days`
+    days_reason.innerHTML = `📆 That's ${calculatedImpact} days without your ${goal_reason}`
+    context.innerHTML = `Plus, ${cost} is equivalent to ${coffees} coffees`;
+    visual.innerHTML = object.repeat(coffees);
   });
 }
 
