8de5788420dfc0032753a177d04b4deb2bb2fcc0
Kym hatran <kym.hatran@gmail.com>
2021-07-24 11:19:16 +1000

    calculator updated

--- app/javascript/packs/purchase_creator.js
+++ app/javascript/packs/purchase_creator.js
@@ -1,33 +1,33 @@
 const purchaseForm = document.getElementById("purchase_form");
-const weeklyContribution = document.getElementById("weekly-contribution").innerHTML;
 
 
 function calculator(){
-  purchaseForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const impact = document.getElementById("impact")
-    const setback = document.getElementById("setback")
-    const context = document.getElementById("context")
-    const visual = document.getElementById("visual")
-    const goal_name = document.getElementById("goal_name").innerHTML
-    const goal_reason = document.getElementById("goal_reason").value
+    const weeklyContribution = document.getElementById("weekly-contribution").innerHTML;
+    purchaseForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const impact = document.getElementById("impact")
+      const setback = document.getElementById("setback")
+      const context = document.getElementById("context")
+      const visual = document.getElementById("visual")
+      // const goal_name = document.getElementById("select").innerHTML
+      // const goal_reason = document.getElementById("goal_reason").value
 
-    const days_reason = document.getElementById("days_reason")
-    const purchase = purchaseForm.querySelectorAll("input")[0].value
-    const cost = purchaseForm.querySelectorAll("input")[1].value
+      const days_reason = document.getElementById("days_reason")
+      const purchase = purchaseForm.querySelectorAll("input")[0].value
+      const cost = purchaseForm.querySelectorAll("input")[1].value
 
-    const calculatedImpact = (parseInt(cost) / parseInt(weeklyContribution)) * 7
+      const calculatedImpact = (parseInt(cost) / parseInt(weeklyContribution)) * 7
 
-    const coffees= (parseInt(cost) / 5)
-    const object= `☕️`;
+      const coffees= (parseInt(cost) / 5)
+      const object= `☕️`;
 
 
-    impact.innerHTML = `If you buy this ${purchase} for $${cost}:`
-    setback.innerHTML = `⭐️ You won’t achieve your ${goal_name} for another ${calculatedImpact} days`
-    days_reason.innerHTML = `📆 That's ${calculatedImpact} days without your ${goal_reason}`
-    context.innerHTML = `Plus, ${cost} is equivalent to ${coffees} coffees`;
-    visual.innerHTML = object.repeat(coffees);
-  });
+      impact.innerHTML = `If you buy this ${purchase} for $${cost}:`
+      setback.innerHTML = `⭐️ You won’t achieve your {goal_name} for another ${calculatedImpact} days`
+      days_reason.innerHTML = `📆 That's ${calculatedImpact} days without your {goal_reason}`
+      context.innerHTML = `Plus, ${cost} is equivalent to ${coffees} coffees`;
+      visual.innerHTML = object.repeat(coffees);
+    });
 }
 
 // function calculateImpact(){
