2f6616d03b8e466dda2e316883d079ec37e1e74b
Jay Reidy <jaylreidy21@gmail.com>
2021-07-24 13:09:52 +1000

    Added weekly contributions to your goals and also you're now unable to add 2 goals on 1 account

---
+++ app/helpers/contribution_helper.rb
@@ -0,0 +1,23 @@
+module ContributionHelper
+  def account_balance(goal, user)
+    value = 0
+    date = ""
+    accounts = UpApi.accounts(user)
+    accounts.each do |account|
+      if account[1][:display_name] == goal.name
+        value = account[1][:value]
+      end
+    end
+    value
+  end
+
+  def weekly_contributions(goal, user)
+    value = account_balance(goal, user)
+    date_today = Time.now.strftime("%d/%m/%Y")
+    no_weeks = (((Date.parse(goal.due_date)-Date.parse(date_today)).to_i) / 7).floor
+    if no_weeks < 1
+      no_weeks = 1
+    end
+    ((goal.amount - value.to_i) / no_weeks)
+  end
+end
