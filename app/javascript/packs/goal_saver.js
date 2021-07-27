461e4feba048c9949c2c7edc0e6eea6f6053b72d
Jay Reidy <jaylreidy21@gmail.com>
2021-07-27 18:34:36 +1000

    Made goal_saver.js into a function

--- app/javascript/packs/goal_saver.js
+++ app/javascript/packs/goal_saver.js
@@ -1,38 +1,33 @@
 import { format } from 'date-fns'
-document.addEventListener("turbolinks:load", (event) => {
+function goalSaver() {
+  const goalForm = document.getElementById("goal_form")
+    if (goalForm) {
+      const selectElement = document.querySelector('#goal_form');
+      const todaysDate = format(new Date(), 'dd/MM/yyyy');
+      const selectedDate = document.querySelector('#goal_due_date');
 
-  if (document.getElementById('goal_form')) {
-    const selectElement = document.querySelector('#goal_form');
+      function parseDate(str) {
+          var mdy = str.split('/');
+          return new Date(mdy[2], mdy[1]-1, mdy[0]);
+      }
 
-    selectElement.addEventListener('change', (event) => {
-      const result = document.querySelector('#result');
-      result.textContent = selectElement.options[selectElement.selectedIndex].dataset.balance;
-    });
+      function datediff(first, second) {
+          return Math.round((second-first)/(1000*60*60*24));
+      }
 
+      selectElement.addEventListener('change', (event) => {
+        const result = document.querySelector('#result');
+        result.textContent = selectElement.options[selectElement.selectedIndex].dataset.balance;
+      });
 
-    const todaysDate = format(new Date(), 'dd/MM/yyyy')
-    console.log(todaysDate)
-
-
-
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[1]-1, mdy[0]);
-    }
-
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    }
-
-    const selectedDate = document.querySelector('#goal_due_date');
-
-    selectedDate.addEventListener('change', (event) => {
-      const balance = parseInt(document.querySelector('#result').innerText);
-      const goal_amount = parseFloat(document.querySelector('#amount').value);
-      const weeks = document.querySelector('#weeks');
-      const noWeeks = Math.floor(datediff(parseDate(todaysDate), parseDate(selectedDate.value)) / 7);
-      const amount = Math.round((goal_amount - balance) / noWeeks);
-      weeks.textContent = amount;
+      selectedDate.addEventListener('change', (event) => {
+        const balance = parseInt(document.querySelector('#result').innerText);
+        const goal_amount = parseFloat(document.querySelector('#amount').value);
+        const weeks = document.querySelector('#weeks');
+        const noWeeks = Math.floor(datediff(parseDate(todaysDate), parseDate(selectedDate.value)) / 7);
+        const amount = Math.round((goal_amount - balance) / noWeeks);
+        weeks.textContent = amount;
     });
   };
-});
+}
+export {goalSaver};
